51208594cd6799febc9f3ec59aab8d4510c2bb2c
unknown <Legioner@.(none)>
2010-11-28 13:41:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NotifierHelper::Application.config.secret_token = '03f342c34a68a6274c3d5410c43658c03e7aa5a1f126fc9d25641471266400a119e8b467c4f8bfc480a8bc2af9fa1708541d4c912ff67dd9ace291d9dd96abc0'
